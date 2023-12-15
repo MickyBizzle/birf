7ba490fce2d9a776bf89396e986a18f41e2f3c01
mickybizzle <ballardm123@gmail.co.uk>
2023-12-15 15:47:48 +0000

    update image url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["dev.backend.birf.com"],
+    domains: ["dev.backend.birf.com", "backend.birf.co.uk"],
   },
 };
 
